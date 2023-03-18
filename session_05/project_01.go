f91dc534f97227d5146004324bdf800201dd64c6
fatonianggris <mfatonianggris@gmail.com>
2023-03-18 14:44:07 +0700

    project 01

--- session_05/project_01.go
+++ session_05/project_01.go
@@ -17,10 +17,7 @@ func processAcak2(data interface{}, no int) {
 	fmt.Println(data, " ", no)
 }
 
-func cetakAcak() {
-
-	data1 := []interface{}{"bisa1", "bisa2", "bisa3"}
-	data2 := []interface{}{"coba1", "coba2", "coba3"}
+func cetakAcak(data1 interface{}, data2 interface{}) {
 
 	for i := 1; i <= 4; i++ {
 		go processAcak1(data1, i)
@@ -42,10 +39,7 @@ func processRapi2(data interface{}, no int) {
 	mutex.Unlock()
 }
 
-func cetakRapi() {
-
-	data1 := []interface{}{"bisa1", "bisa2", "bisa3"}
-	data2 := []interface{}{"coba1", "coba2", "coba3"}
+func cetakRapi(data1 interface{}, data2 interface{}) {
 
 	for i := 1; i <= 4; i++ {
 		wg.Add(2)
@@ -58,10 +52,13 @@ func cetakRapi() {
 
 func main() {
 
+	data1 := []interface{}{"bisa1", "bisa2", "bisa3"}
+	data2 := []interface{}{"coba1", "coba2", "coba3"}
+
 	fmt.Println("======================= ACAK ===========================")
-	cetakAcak()
+	cetakAcak(data1, data2)
 	time.Sleep(3 * time.Second)
 	fmt.Println("======================= RAPI ===========================")
-	cetakRapi()
+	cetakRapi(data1, data2)
 	time.Sleep(3 * time.Second)
 }
